02fe35ebc82faf9d00621f30da8da39413da91c7
Joseph Mwangi <korimwangi01@gmail.com>
2025-09-26 10:21:39 -0400

    trigger deployment

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,5 @@ import "./index.css";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
+// Trigger deployment
 createRoot(document.getElementById("root")).render(<App />);
